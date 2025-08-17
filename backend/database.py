e2af8b633586d91edab1fe1faeccd7564c785dea
harshadChavare <harshadchavare6@gmail.com>
2025-08-17 21:05:57 +0530

    db changed

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,17 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DB_URL = "mysql+pymysql://Harshad:Harshad@localhost/groccery"  # Replace with your real password
+# DB_URL = "mysql+pymysql://Harshad:Harshad@localhost/groccery"  # mysql
+# database.py
+
+# postgres
+DB_URL = "postgresql://postgres:123@localhost:5432/groccery"  # Change this line
+# DATABASE_URL = "postgresql://postgres:123@localhost:5432/dbname"
+
+
+# Example:
+# DB_URL = "postgresql://Harshad:Harshad@localhost:5432/groccery"
+
 
 engine = create_engine(DB_URL)
 # SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
