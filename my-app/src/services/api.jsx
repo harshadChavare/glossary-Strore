da97defcb127b99a4ac64729b824ccb1ae7e844f
harshadChavare <harshadchavare6@gmail.com>
2025-09-14 23:48:00 +0530

    seed updated

--- my-app/src/services/api.jsx
+++ my-app/src/services/api.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8000/api';
+// const API_BASE_URL = 'http://localhost:8000/api';
+const API_BASE_URL = 'https://glossary-strore.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
